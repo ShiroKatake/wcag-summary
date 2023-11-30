c7350bf86a89804b3e7a49994357cf5665ce17c2
ShiroKatake <tgkhang262@gmail.com>
2023-11-30 23:08:29 +1100

    fix(doc structure): added ordering to the documents

--- cms/sanity.config.ts
+++ cms/sanity.config.ts
@@ -10,7 +10,54 @@ export default defineConfig({
   projectId: 'qq18jngx',
   dataset: 'production',
 
-  plugins: [deskTool(), visionTool()],
+  plugins: [deskTool({
+    structure: (S) =>
+    S.list()
+    .title("Content")
+    .items([
+      S.listItem()
+        .title("Criterions")
+        .id("criterion")
+        .schemaType("criterion")
+        .child(
+          S.documentList()
+            .title("Criterions")
+            .filter('_type == "criterion"')
+            .defaultOrdering([
+              {
+                field: 'guidelineRef',
+                direction: 'desc'
+              },
+            ])
+        ),
+      S.listItem()
+        .title("Guidelines")
+        .id("guideline")
+        .schemaType("guideline")
+        .child(
+          S.documentList()
+            .title("Guidelines")
+            .filter('_type == "guideline"')
+            .defaultOrdering([{
+              field: 'id',
+              direction: 'asc'
+            }])
+        ),
+      S.listItem()
+        .title("Principles")
+        .id("principle")
+        .schemaType("principle")
+        .child(
+          S.documentList()
+            .title("Principles")
+            .filter('_type == "principle"')
+            .defaultOrdering([{
+              field: 'id',
+              direction: 'asc'
+            }])
+        ),
+      ]),
+  }), visionTool()],
 
   schema: {
     types: schemaTypes,
