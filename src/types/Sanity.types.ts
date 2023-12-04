6cbeb6b635233b04143ef1470d3876ca18166217
ShiroKatake <tgkhang262@gmail.com>
2023-12-04 14:13:15 +1100

    feat: sanity generic types

---
+++ src/types/Sanity.types.ts
@@ -0,0 +1,12 @@
+export type SanityDoc = {
+  _createdAt: string
+  _id: string
+  _rev: string
+  _type: string
+  _updatedAt: string
+}
+
+export type SanityRef = {
+  _ref: string
+  _type: string
+}
